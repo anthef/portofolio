08763d55c74ad3b07c4c3791e6fdba75f2238e9e
anthef <anthonyef09@gmail.com>
2025-01-19 16:35:26 +0700

    deploy error

--- src/components/modules/landingmodule/about/SkillSlider.tsx
+++ src/components/modules/landingmodule/about/SkillSlider.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import { skillsList } from './skillsList';
+import { skillsList } from './skillslist';
 
 interface SkillSliderProps {
   direction?: 'left' | 'right';
